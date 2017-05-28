fccd02fea4820c2911e296f289a6377986681e54
Hung Nguyen <nguyendanghung2008@gmail.com>
2017-05-28 01:34:15 +0100

    Update config-cluster.sh

--- config-cluster.sh
+++ config-cluster.sh
@@ -3,3 +3,11 @@ kubectl config set-context openiotsuper-system --cluster=openiotsuper-cluster
 kubectl config use-context openiotsuper-system
 kubectl get nodes
 
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+
+kubectl apply -f [podnetwork].yaml
+
+
+
