9b267589586a53336a8849a5a31bdba60b18bd24
Hung Nguyen <nguyendanghung2008@gmail.com>
2017-05-28 10:31:45 +0100

    Update config-cluster.sh

--- config-cluster.sh
+++ config-cluster.sh
@@ -1,13 +1,17 @@
-kubectl config set-cluster openiotsuper-cluster --server=http://54.210.204.155:8080
-kubectl config set-context openiotsuper-system --cluster=openiotsuper-cluster
-kubectl config use-context openiotsuper-system
-kubectl get nodes
+
 
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
-kubectl apply -f [podnetwork].yaml
+kubectl apply -f https://git.io/weave-kube-1.6
 
 
+kubectl get pods --all-namespaces | grep dashboard
+
+kubectl create -f https://git.io/kube-dashboard
+
+
+
+kubectl get nodes
 
